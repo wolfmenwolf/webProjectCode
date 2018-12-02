58046befcbfff2a2d41c711d957ed0877980d7ac
qinhao <280442162@qq.com>
2018-12-02 14:41:35 +0800

    author:qinhao time:20181202 14:41 action:first web version

--- zl/网站重构&web标准设计视频（第二版视频教程目录）/4.3/duote/js/qiecss.js
+++
@@ -1,9 +0,0 @@
-function e(i){
-	var i;
-	document.getElementById("tabUpdate").className="tabUpdateD"+i;
-	window.name=i;//window.name°æ
-}
-onload=function(){
-	var a=document.links;
-	for(var i=0;i<a.length;i++)a[i].onfocus=function(){this.blur();}
-}
